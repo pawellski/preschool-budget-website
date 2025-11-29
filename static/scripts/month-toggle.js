8291c87a20fffd86331496362622e88b1c926f98
Paweł Skiba <skibaapawell@gmail.com>
2025-11-29 01:40:18 +0100

    feat: first version of file data loader

--- static/scripts/month-toggle.js
+++ static/scripts/month-toggle.js
@@ -1,15 +1,17 @@
-document.querySelectorAll('.month').forEach(month => {
-    const header = month.querySelector('.month-header');
-    const content = month.querySelector('.month-content');
-    const arrow = header.querySelector('.arrow');
-    
-    header.addEventListener('click', () => {
-        content.classList.toggle('open');
-        arrow.classList.toggle('open');
-        if(content.classList.contains('open')) {
-            content.style.maxHeight = content.scrollHeight + 'px';
-        } else {
-            content.style.maxHeight = '0';
-        }
+function initMonthToggle() {
+    document.querySelectorAll('.month').forEach(month => {
+        const header = month.querySelector('.month-header');
+        const content = month.querySelector('.month-content');
+        const arrow = header.querySelector('.arrow');
+        
+        header.addEventListener('click', () => {
+            content.classList.toggle('open');
+            arrow.classList.toggle('open');
+            if (content.classList.contains('open')) {
+                content.style.maxHeight = content.scrollHeight + 'px';
+            } else {
+                content.style.maxHeight = '0';
+            }
+        });
     });
-});
+}
